3587f7e6300eb7502a716a4474956cddafa5df2c
clarkjrcanlog <clarkcanlog@gmail.com>
2019-02-04 14:40:59 +0800

    working array

--- array.php
+++ array.php
@@ -5,8 +5,17 @@
     <body>
         <?php
             $age = [
-                
+                "Peter" => "35",
+                "Ben" => "37",
+                "Joe" => "43"
             ];
         ?>
+        
+        <ul>
+            <?php foreach($age as $pname => $page) : ?>
+                <li>Name= <?= $pname . ', '?>    Age= <?= $page?></li>
+            <?php endforeach ; ?>
+
+        </ul>
     </body>
 </html>
