8fb4716363bb757372ab357cca8e9d89e42437e8
clarkjrcanlog <clarkcanlog@gmail.com>
2019-02-04 14:16:34 +0800

    added initial files

---
+++ array.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>PHP ARRAY</title>
+    </head>
+    <body>
+        <?php
+            $age = [
+                
+            ];
+        ?>
+    </body>
+</html>
