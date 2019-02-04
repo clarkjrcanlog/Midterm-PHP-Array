57b4057652e764f1ce53cb28b935e6ca5382908a
clarkjrcanlog <clarkcanlog@gmail.com>
2019-02-04 14:45:29 +0800

    added view file

--- array.php
+++ array.php
@@ -1,21 +1,7 @@
-<html>
-    <head>
-        <title>PHP ARRAY</title>
-    </head>
-    <body>
-        <?php
+<?php
             $age = [
                 "Peter" => "35",
                 "Ben" => "37",
                 "Joe" => "43"
             ];
         ?>
-        
-        <ul>
-            <?php foreach($age as $pname => $page) : ?>
-                <li>Name= <?= $pname . ', '?>    Age= <?= $page?></li>
-            <?php endforeach ; ?>
-
-        </ul>
-    </body>
-</html>
